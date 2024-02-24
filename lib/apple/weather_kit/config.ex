9c945830bc6b23aa9a4fa026b2b6ba1e576a38ef
c4710n <c4710n@users.noreply.github.com>
2024-02-24 20:14:49 +0800

    feat: add module for building config

---
+++ lib/apple/weather_kit/config.ex
@@ -0,0 +1,44 @@
+defmodule Apple.WeatherKit.ConfigError do
+  defexception [:message]
+end
+
+defmodule Apple.WeatherKit.Config do
+  @schema [
+    team_id: [
+      type: :string,
+      required: true
+    ],
+    service_id: [
+      type: :string,
+      required: true
+    ],
+    key_id: [
+      type: :string,
+      required: true
+    ],
+    private_key: [
+      type: :string,
+      required: true
+    ]
+  ]
+
+  @enforce_keys Keyword.keys(@schema)
+  defstruct @enforce_keys
+
+  def new(options) when is_list(options) do
+    case NimbleOptions.validate(options, @schema) do
+      {:ok, validated_options} ->
+        {:ok, struct!(__MODULE__, validated_options)}
+
+      {:error, %NimbleOptions.ValidationError{message: message}} ->
+        {:ok, %Apple.WeatherKit.ConfigError{message: message}}
+    end
+  end
+
+  def new!(options) when is_list(options) do
+    case new(options) do
+      {:ok, config} -> config
+      {:error, error} -> raise error
+    end
+  end
+end
