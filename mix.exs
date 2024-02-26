c63611ac76c18e52900b6f062788135918771e28
c4710n <c4710n@users.noreply.github.com>
2024-02-26 11:14:32 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Apple.WeatherKit.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "An client for Apple's WeatherKit REST API."
   @source_url "https://github.com/elixir-apple/apple_weather_kit"
 
